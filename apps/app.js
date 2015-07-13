cb9bc67694efd8f64a6e4bb0c23287598199e205
jamilabreu <abreu.jamil@gmail.com>
2015-07-12 23:59:32 -0700

    suggestions

--- apps/app.js
+++ apps/app.js
@@ -1,93 +1,49 @@
 $(function(){
 	var page_number = 1
+
 	getRequest(page_number);
-	$("#next").on("click", function () {
-		page_number +=1
+
+	$('#next').on('click', function() {
+		page_number +=1;
 		getRequest(page_number);
 	});
-	$("#prev").on("click", function () {
-		if (page_number==1){
+
+	$('#prev').on('click', function() {
+		if (page_number==1) {
 			return;
-		}else{
-			page_number -=1
+		} else {
+			page_number -=1;
 			getRequest(page_number);
 		}
 	});
-	
-	
-	
-	
 
 	function getRequest(page_number) {
-		//	debugger;
 		var params = {
 			api_key: 'ebea8cfca72fdff8d2624ad7bbf78e4c',
 			page: page_number
 		};
-		url = 'http://api.themoviedb.org/3/movie/now_playing';
+
+		var url = 'http://api.themoviedb.org/3/movie/now_playing';
+
 		$.getJSON(url, params).done(function(data) {
 			showResults(data.results);
 		});
 	}
 
-	function showResults(results){
-		var html = "";
+	function showResults(results) {
+		var html = '';
 		$.each(results, function(index,value){
 			console.log(value.title);
 			poster_path = value.poster_path;
 			poster_title = value.title;
-			if (poster_path==null){
+			if (poster_path == null) {
 				return;
-			}
-			else {
-				html += '<div class="single_poster crop"><img src="http://image.tmdb.org/t/p/w154' + poster_path  + '"  title="' + poster_title +'"/></div>';
-				$('#movie_posters').html(html);
-
+			} else {
+				html += '<div class="poster"><img src="http://image.tmdb.org/t/p/w154' + poster_path  + '"  title="' + poster_title +'"/></div>';
+				$('#posters').html(html);
 			}
 		});
-		$('#movie_posters').html(html);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-//
-//
-//	url = 'http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&page=2';
-//
-//	$.getJSON(url, function(data){
-//		myData = data.results;
-//		var html = "";
-//
-//		$.each(myData, function(index, value){
-//			console.log(value.title);
-//			poster_path = value.poster_path;
-//			poster_title = value.title;
-//			if (poster_path==null){
-//				return;
-//			}
-//			else {
-//				html += '<div class="single_poster"><img src="http://image.tmdb.org/t/p/w154' + poster_path  + '"  title="' + poster_title +'"/></div>';
-//				$('#movie_posters').html(html);
-//
-//			}
-//
-//		});
-//	});
-
-
-
 });
 
 
